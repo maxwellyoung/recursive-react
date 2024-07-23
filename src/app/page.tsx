541282dc88d3373feb284800841afffad3dae03e
Maxwell Young <maxtheyoung@gmail.com>
2024-07-23 16:34:44 +1200

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,60 @@
-import { FolderIcon } from "@heroicons/react/24/solid";
+"use client";
+import {
+  ChevronRightIcon,
+  DocumentIcon,
+  FolderIcon,
+} from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+type Folder = {
+  name: string;
+  folders?: Folder[];
+};
+
+let folders: Folder[] = [
+  {
+    name: "Home",
+    folders: [
+      {
+        name: "Movies",
+        folders: [
+          {
+            name: "Action",
+            folders: [
+              {
+                name: "2000s",
+                folders: [
+                  { name: "Gladiator.mp4" },
+                  { name: "American-Beauty.mp4" },
+                ],
+              },
+              { name: "2010s", folders: [] },
+            ],
+          },
+          {
+            name: "Comedy",
+            folders: [
+              { name: "2000s", folders: [] },
+              { name: "2010s", folders: [] },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Music",
+        folders: [
+          { name: "Pop", folders: [] },
+          { name: "Rock", folders: [] },
+        ],
+      },
+      { name: "Picture", folders: [] },
+      { name: "Documents", folders: [] },
+      { name: "passwords.txt" },
+    ],
+  },
+];
 
 export default function Home() {
-  let folders = [
-    {
-      name: "Home",
-      folders: [
-        {
-          name: "Movies",
-          folders: [
-            {
-              name: "Action",
-              folders: [
-                { name: "2000s", folders: [{ name: "Popular " }] },
-                { name: "2010s" },
-              ],
-            },
-            { name: "Comedy", folders: [{ name: "2000s" }, { name: "2010s" }] },
-          ],
-        },
-        { name: "Music", folders: [{ name: "Pop" }, { name: "Rock" }] },
-        { name: "Picture" },
-        { name: "Documents" },
-      ],
-    },
-  ];
-
   return (
     <div className="p-8 max-w-sm mx-auto">
       <ul>
@@ -38,18 +68,37 @@ export default function Home() {
   );
 }
 
-function Folder({ folder }) {
+function Folder({ folder }: { folder: Folder }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <li className="my-1.5" key={folder.name}>
       <span className="flex items-center gap-1.5">
-        <FolderIcon className="size-6 text-sky-500" />
+        {folder.folders && folder.folders.length > 0 && (
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <ChevronRightIcon
+              className={`size-4 text-gray-500 ${!isOpen ? "" : "rotate-90"}`}
+            />
+          </button>
+        )}
+        {folder.folders ? (
+          <FolderIcon
+            className={`size-6 text-sky-500 ${
+              folder.folders.length === 0 ? "ml-[22px]" : ""
+            }`}
+          />
+        ) : (
+          <DocumentIcon className=" ml-[22px] size-6 text-gray-900" />
+        )}
         {folder.name}
       </span>
-      <ul className="pl-6">
-        {folder.folders?.map((folder) => (
-          <Folder folder={folder} key={folder.name} />
-        ))}
-      </ul>
+      {isOpen && (
+        <ul className="pl-6">
+          {folder.folders?.map((folder) => (
+            <Folder folder={folder} key={folder.name} />
+          ))}
+        </ul>
+      )}
     </li>
   );
 }
